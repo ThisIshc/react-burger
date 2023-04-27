8f73ec9ceff8e592d7e5270995dae87d6ac7db89
andreybayandin <andreyy.bayandin@gmail.com>
2023-04-27 23:15:22 +0500

    gh-pages fix

--- src/index.tsx
+++ src/index.tsx
@@ -13,7 +13,7 @@ const root = ReactDOM.createRoot(
 root.render(
   <React.StrictMode>
     <Provider store={store}>
-        <Router>
+        <Router basename={process.env.PUBLIC_URL}>
             <App />
         </Router>
     </Provider>
